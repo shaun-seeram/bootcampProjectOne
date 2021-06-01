7b6f713e4ec80e62fa890a59653678a8eddb5af0
Shaun S <shaunseeram@hotmail.com>
2021-05-31 23:19:09 -0400

    Fixed JS

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function click() {
-    const menu = document.querySelector(".subMenu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex"
-    } else {
-        menu.style.display = "none"
+const burger = document.querySelector("nav");
+
+console.log(burger)
+
+burger.addEventListener("click", function(e) {
+    if (e.target.tagName === "svg") {
+        const menu = document.querySelector(".subMenu");
+        if (menu.style.display === "none" || menu.style.display === "") {
+            menu.style.display = "flex"
+        } else {
+            menu.style.display = "none"
+        }
     }
-}
+})
